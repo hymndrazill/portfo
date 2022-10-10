985facea12ed8792a00633c417425cfe347d941b
hymndrazill <srihafiras@yahoo.fr>
2022-10-10 17:46:39 +0100

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import {FaGithub} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'> Hymndrazill</a>
+      <a href="#" className='footer__logo'> </a>
         <ul className='permalinks'>
           <li> <a href="#">Home</a></li>
           <li> <a href="#about">About</a></li>
