ca848bc6a80dfc0e2bf28a34971164ca0c64f97d
hymndrazill <srihafiras@yahoo.fr>
2023-03-16 22:54:07 +0100

    changed res

--- src/components/header/Buttons.jsx
+++ src/components/header/Buttons.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import FirasSriha from "../../assets/FirasSriha.pdf"
+import Firas_sriha_resume from "../../assets/Firas_sriha_resume.pdf"
 const Buttons = () => {
   return (
     <div className='Button'>
-        <a href={FirasSriha} className="btn" download >Download CV</a>
+        <a href={Firas_sriha_resume.pdf} className="btn" download >Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact Me.</a>
     </div>
   )
